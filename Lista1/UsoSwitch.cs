970cd4d8c756b70917ea88cd057b61edf4a768e7
Matheuslagos <60862073+Matheuslagos@users.noreply.github.com>
2022-06-30 22:01:23 -0300

    Create UsoSwitch.cs

---
+++ Lista1/UsoSwitch.cs
@@ -0,0 +1,18 @@
+using System;
+namespace estudos
+{
+  Class MainClass
+  {
+Console.WriteLine ("digite num do mes");
+        int x = int.Parse(Console.ReadLine());
+        switch (x)
+        {
+            case 1:Console.WriteLine("janeiro");break;
+            case 2: Console.WriteLine("fev"); break;
+            case 3: Console.WriteLine("marco"); break;
+            default: Console.WriteLine("mes invlaido");break
+        }
+            
+   }     
+}
+            
