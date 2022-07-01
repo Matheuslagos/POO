0709fbe1738fd90c127bc697bfb76b0afef9909e
Matheuslagos <60862073+Matheuslagos@users.noreply.github.com>
2022-07-01 03:05:36 -0300

    Create GetAndSet.cs

---
+++ Lista1/GetAndSet.cs
@@ -0,0 +1,50 @@
+using System;
+namespace estudos
+{
+
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+            Triangulo x = new Triangulo();
+
+            x.SetBase(10);
+            x.SetAltura(20);
+
+            Console.WriteLine(x.CalcArea());
+            Triangulo y = new Triangulo();
+            y.SetBase(10);
+            y.SetAltura(20);
+            
+            Console.WriteLine(y.CalcArea());
+        }
+    }
+    class Triangulo
+    {
+        private double b, h;
+        public void SetBase(double v)
+        {
+            if (v>0) b = v;
+        }
+        public void SetAltura (double v)
+        {
+            if (v>0) h = v;
+        }
+        public double GetBase()
+        {
+            return b;
+        }
+        public double GetAltura()
+        {
+            return h;
+        }
+      
+        public double CalcArea()
+        {
+            double area = b * h / 2;
+            return area;
+        }
+
+    }
+}
