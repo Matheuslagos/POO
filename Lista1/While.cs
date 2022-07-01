fa5c92d33359a71357a75af84b6bed54a4cc70b9
Matheuslagos <60862073+Matheuslagos@users.noreply.github.com>
2022-06-30 22:02:52 -0300

    Create While.cs

---
+++ Lista1/While.cs
@@ -0,0 +1,20 @@
+using System;
+namespace estudos
+{
+    
+    class MainClass
+    {
+        
+    int s = 0;
+        int n = 1;
+        while (n < 20)
+        {
+            Console.WriteLine(n);
+            s += n;
+            n += 2;
+        }
+        Console.WriteLine(s);
+        }
+    }
+  
+}
