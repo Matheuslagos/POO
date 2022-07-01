2a83e252a7924972e5bb4addf4652ef4c3f323c9
Matheuslagos <60862073+Matheuslagos@users.noreply.github.com>
2022-06-30 21:44:57 -0300

    Create Lista1Q1.cs

---
+++ Lista1Q1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace estudos 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite seu primeiro nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Bem vindo ao c#," + nome);
+
+        }
+    }
+}
